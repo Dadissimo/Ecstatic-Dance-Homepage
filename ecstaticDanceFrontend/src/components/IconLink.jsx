65d951ed8e5ada36abc2139eed1173a35bb2f472
Dragan Runjaic <dado.runjaic@gmail.com>
2024-03-30 13:03:08 +0100

    underline link

--- ecstaticDanceFrontend/src/components/IconLink.jsx
+++ ecstaticDanceFrontend/src/components/IconLink.jsx
@@ -5,7 +5,7 @@ const IconLink = ({icon, text, url}) => {
     return (
         <div className='m-0.5'>
             <FontAwesomeIcon fixedWidth color='white' size='xl' icon={icon} />
-            <a href={url} className='ml-2 text-white'>
+            <a href={url} className='ml-2 text-white underline'>
                 {text}
             </a>
         </div>
